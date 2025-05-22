60dfc70551052f6bd77fcf491285cd05927c235e
Oskillar21 <oscar.calvosiles@ucr.ac.cr>
2025-05-22 17:58:04 -0600

    Cambio de ruta por utilizacion del Docker

--- services/sender.py
+++ services/sender.py
@@ -4,7 +4,7 @@ import requests
 
 def send_embedding_to_api(document_id: str, embedding: list[float], metadata: dict):
     #Cambio en esta linea para que me funcione con la otra api 
-    url = "http://api-load:8002/store_embedding" 
+    url = "http://api-load:8000/store_embedding" 
     payload = {
         "id": document_id,
         "embedding": embedding,
@@ -14,7 +14,6 @@ def send_embedding_to_api(document_id: str, embedding: list[float], metadata: di
     print(f"🧠 Longitud del embedding: {len(embedding)}")
     print(f"📦 Metadata: {metadata}")
 
-    
     response = requests.post(url, json=payload)
     response.raise_for_status()  # Lanza excepción si hay error HTTP
     return response.json() # Devuelve la respuesta de la API
